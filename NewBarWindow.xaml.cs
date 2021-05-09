03ef391f42a96a6805b67a9e70c917126ea9e871
Ondřej Samsounek <samsounek.2018@skola.ssps.cz>
2021-05-09 11:44:24 +0200

    clear NewBarWindow added, in settings window veryfication works but displaying isn't triggerd

---
+++ NewBarWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TimeOrganiser
+{
+    /// <summary>
+    /// Interaction logic for NewBarWindow.xaml
+    /// </summary>
+    public partial class NewBarWindow : Window
+    {
+        public NewBarWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
